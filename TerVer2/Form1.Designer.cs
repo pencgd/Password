01bc8f821b518d684089afabe967a930b813ecf4
pencgd <iskandarovdanis@gmail.com>
2026-05-06 21:39:12 +0500

    Add files via upload

---
+++ TerVer2/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿using System.Drawing;
+
+namespace TerVer2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Panel txtBorder;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStrength = new System.Windows.Forms.Label();
+            this.lblCrackTime = new System.Windows.Forms.Label();
+            this.progressStrength = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGeneratePassword = new System.Windows.Forms.Button();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.txtBorder = new System.Windows.Forms.Panel();
+            this.txtBorder.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(28)))));
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.txtPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtPassword.Location = new System.Drawing.Point(9, 3);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(412, 32);
+            this.txtPassword.TabIndex = 0;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.label1.Location = new System.Drawing.Point(50, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(462, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Проверка надёжности пароля";
+            // 
+            // lblStrength
+            // 
+            this.lblStrength.AutoSize = true;
+            this.lblStrength.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblStrength.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(102)))), ((int)(((byte)(241)))));
+            this.lblStrength.Location = new System.Drawing.Point(250, 178);
+            this.lblStrength.Name = "lblStrength";
+            this.lblStrength.Size = new System.Drawing.Size(38, 32);
+            this.lblStrength.TabIndex = 2;
+            this.lblStrength.Text = "—";
+            this.lblStrength.Click += new System.EventHandler(this.lblStrength_Click);
+            // 
+            // lblCrackTime
+            // 
+            this.lblCrackTime.AutoSize = true;
+            this.lblCrackTime.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.lblCrackTime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.lblCrackTime.Location = new System.Drawing.Point(250, 218);
+            this.lblCrackTime.Name = "lblCrackTime";
+            this.lblCrackTime.Size = new System.Drawing.Size(38, 32);
+            this.lblCrackTime.TabIndex = 3;
+            this.lblCrackTime.Text = "—";
+            // 
+            // progressStrength
+            // 
+            this.progressStrength.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(28)))));
+            this.progressStrength.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(102)))), ((int)(((byte)(241)))));
+            this.progressStrength.Location = new System.Drawing.Point(50, 280);
+            this.progressStrength.Name = "progressStrength";
+            this.progressStrength.Size = new System.Drawing.Size(500, 8);
+            this.progressStrength.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(160)))), ((int)(((byte)(170)))));
+            this.label2.Location = new System.Drawing.Point(50, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Уровень надёжности";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(160)))), ((int)(((byte)(170)))));
+            this.label3.Location = new System.Drawing.Point(50, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Время взлома";
+            // 
+            // btnGeneratePassword
+            // 
+            this.btnGeneratePassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(102)))), ((int)(((byte)(241)))));
+            this.btnGeneratePassword.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGeneratePassword.FlatAppearance.BorderSize = 0;
+            this.btnGeneratePassword.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGeneratePassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btnGeneratePassword.ForeColor = System.Drawing.Color.White;
+            this.btnGeneratePassword.Location = new System.Drawing.Point(50, 320);
+            this.btnGeneratePassword.Name = "btnGeneratePassword";
+            this.btnGeneratePassword.Size = new System.Drawing.Size(511, 50);
+            this.btnGeneratePassword.TabIndex = 7;
+            this.btnGeneratePassword.Text = "Сгенерировать пароль";
+            this.btnGeneratePassword.UseVisualStyleBackColor = false;
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(28)))));
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(600, 80);
+            this.panelHeader.TabIndex = 8;
+            // 
+            // panelMain
+            // 
+            this.panelMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(35)))));
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(0, 80);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(600, 340);
+            this.panelMain.TabIndex = 9;
+            // 
+            // txtBorder
+            // 
+            this.txtBorder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(102)))), ((int)(((byte)(241)))));
+            this.txtBorder.Controls.Add(this.txtPassword);
+            this.txtBorder.Location = new System.Drawing.Point(48, 124);
+            this.txtBorder.Name = "txtBorder";
+            this.txtBorder.Size = new System.Drawing.Size(434, 41);
+            this.txtBorder.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(35)))));
+            this.ClientSize = new System.Drawing.Size(600, 420);
+            this.Controls.Add(this.btnGeneratePassword);
+            this.Controls.Add(this.progressStrength);
+            this.Controls.Add(this.lblCrackTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblStrength);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBorder);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.panelHeader);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Password Checker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.txtBorder.ResumeLayout(false);
+            this.txtBorder.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void TxtBorder_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            using (System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath())
+            {
+                int radius = 8;
+                System.Drawing.Rectangle rect = new System.Drawing.Rectangle(0, 0, txtBorder.Width - 1, txtBorder.Height - 1);
+                path.AddArc(rect.X, rect.Y, radius * 2, radius * 2, 180, 90);
+                path.AddArc(rect.Right - radius * 2, rect.Y, radius * 2, radius * 2, 270, 90);
+                path.AddArc(rect.Right - radius * 2, rect.Bottom - radius * 2, radius * 2, radius * 2, 0, 90);
+                path.AddArc(rect.X, rect.Bottom - radius * 2, radius * 2, radius * 2, 90, 90);
+                path.CloseFigure();
+                txtBorder.Region = new System.Drawing.Region(path);
+            }
+        }
+
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStrength;
+        private System.Windows.Forms.Label lblCrackTime;
+        private System.Windows.Forms.ProgressBar progressStrength;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGeneratePassword;
+    }
+}
---
+++ TerVer2/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace TerVer2
+{
+    public partial class Form1 : Form
+    {
+        private static readonly Random random = new Random();
+        private Color accentColor = Color.FromArgb(99, 102, 241);
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializePasswordChecker();
+            InitializeGenerateButton();
+            this.Paint += Form1_Paint;
+            this.Resize += Form1_Resize;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void InitializePasswordChecker()
+        {
+            txtPassword.TextChanged += TxtPassword_TextChanged;
+            progressStrength.Minimum = 0;
+            progressStrength.Maximum = 100;
+            lblStrength.Text = "";
+            lblCrackTime.Text = "";
+            progressStrength.Value = 0;
+        }
+
+        private void InitializeGenerateButton()
+        {
+            btnGeneratePassword.Click += BtnGeneratePassword_Click;
+            btnGeneratePassword.Paint += BtnGeneratePassword_Paint;
+            btnGeneratePassword.MouseEnter += (s, e) => { btnGeneratePassword.BackColor = Color.FromArgb(129, 132, 255); btnGeneratePassword.Invalidate(); };
+            btnGeneratePassword.MouseLeave += (s, e) => { btnGeneratePassword.BackColor = accentColor; btnGeneratePassword.Invalidate(); };
+        }
+
+        private void BtnGeneratePassword_Paint(object sender, PaintEventArgs e)
+        {
+            Button btn = sender as Button;
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            using (GraphicsPath path = GetRoundedRect(btn.ClientRectangle, 12))
+            {
+                btn.Region = new Region(path);
+                using (SolidBrush brush = new SolidBrush(btn.BackColor))
+                {
+                    e.Graphics.FillPath(brush, path);
+                }
+            }
+            TextRenderer.DrawText(e.Graphics, btn.Text, btn.Font, btn.ClientRectangle, btn.ForeColor, TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+            e.Handled = true;
+        }
+
+        private GraphicsPath GetRoundedRect(Rectangle rect, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            int diameter = radius * 2;
+            Rectangle arc = new Rectangle(rect.Location, new Size(diameter, diameter));
+            path.AddArc(arc, 180, 90);
+            arc.X = rect.Right - diameter;
+            path.AddArc(arc, 270, 90);
+            arc.Y = rect.Bottom - diameter;
+            path.AddArc(arc, 0, 90);
+            arc.X = rect.Left;
+            path.AddArc(arc, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+
+        private void BtnGeneratePassword_Click(object sender, EventArgs e)
+        {
+            string newPassword = GenerateStrongPassword();
+            txtPassword.Text = newPassword;
+        }
+
+        private string GenerateStrongPassword()
+        {
+            const string lower = "abcdefghijklmnopqrstuvwxyz";
+            const string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string digits = "0123456789";
+            const string special = "!@#$%^&*()";
+            char[] password = new char[14];
+            password[0] = lower[random.Next(lower.Length)];
+            password[1] = upper[random.Next(upper.Length)];
+            password[2] = digits[random.Next(digits.Length)];
+            password[3] = special[random.Next(special.Length)];
+            string allChars = lower + upper + digits + special;
+            for (int i = 4; i < password.Length; i++)
+            {
+                password[i] = allChars[random.Next(allChars.Length)];
+            }
+            for (int i = 0; i < password.Length; i++)
+            {
+                int j = random.Next(password.Length);
+                char temp = password[i];
+                password[i] = password[j];
+                password[j] = temp;
+            }
+            return new string(password);
+        }
+
+        private void TxtPassword_TextChanged(object sender, EventArgs e)
+        {
+            string password = txtPassword.Text;
+            if (string.IsNullOrEmpty(password))
+            {
+                lblStrength.Text = "Нет пароля";
+                lblCrackTime.Text = "—";
+                progressStrength.Value = 0;
+                UpdateProgressColor(0);
+                return;
+            }
+            var result = EvaluatePassword(password);
+            lblStrength.Text = result.Strength;
+            lblCrackTime.Text = result.CrackTime;
+            progressStrength.Value = result.Score;
+            UpdateProgressColor(result.Score);
+            if (result.Score < 30)
+                lblStrength.ForeColor = Color.FromArgb(239, 68, 68);
+            else if (result.Score < 70)
+                lblStrength.ForeColor = Color.FromArgb(234, 179, 8);
+            else
+                lblStrength.ForeColor = Color.FromArgb(34, 197, 94);
+        }
+
+        private void UpdateProgressColor(int score)
+        {
+            if (score < 30)
+                progressStrength.ForeColor = Color.FromArgb(239, 68, 68);
+            else if (score < 70)
+                progressStrength.ForeColor = Color.FromArgb(234, 179, 8);
+            else
+                progressStrength.ForeColor = Color.FromArgb(34, 197, 94);
+        }
+
+        private PasswordEvaluation EvaluatePassword(string password)
+        {
+            int score = 0;
+            int length = password.Length;
+            if (length >= 12) score += 25;
+            else if (length >= 8) score += 15;
+            else if (length >= 6) score += 5;
+            if (Regex.IsMatch(password, @"\d")) score += 15;
+            if (Regex.IsMatch(password, @"[A-Z]")) score += 15;
+            if (Regex.IsMatch(password, @"[a-z]")) score += 10;
+            if (Regex.IsMatch(password, @"[!@#$%^&*(),.?"":{}|<>]")) score += 20;
+            if (length > 12) score += 15;
+            else if (length > 10) score += 5;
+            if (IsCommonPassword(password)) score -= 30;
+            if (IsSequential(password)) score -= 20;
+            if (HasRepeatingChars(password)) score -= 15;
+            score = Math.Max(0, Math.Min(100, score));
+            string strength;
+            if (score >= 80) strength = "Очень высокая";
+            else if (score >= 60) strength = "Высокая";
+            else if (score >= 40) strength = "Средняя";
+            else if (score >= 20) strength = "Низкая";
+            else strength = "Очень низкая";
+            string crackTime = CalculateCrackTime(password);
+            return new PasswordEvaluation
+            {
+                Score = score,
+                Strength = strength,
+                CrackTime = crackTime
+            };
+        }
+
+        private string CalculateCrackTime(string password)
+        {
+            int length = password.Length;
+            int charsetSize = 0;
+            if (Regex.IsMatch(password, @"[a-z]")) charsetSize += 26;
+            if (Regex.IsMatch(password, @"[A-Z]")) charsetSize += 26;
+            if (Regex.IsMatch(password, @"\d")) charsetSize += 10;
+            if (Regex.IsMatch(password, @"[!@#$%^&*(),.?"":{}|<>]")) charsetSize += 32;
+            if (charsetSize == 0) return "менее секунды";
+            double combinations = Math.Pow(charsetSize, length);
+            double attemptsPerSecond = 1_000_000_000;
+            double seconds = combinations / attemptsPerSecond;
+            if (seconds < 1) return "менее секунды";
+            else if (seconds < 60) return $"{seconds:F1} сек";
+            else if (seconds < 3600) return $"{seconds / 60:F1} мин";
+            else if (seconds < 86400) return $"{seconds / 3600:F1} ч";
+            else if (seconds < 31536000) return $"{seconds / 86400:F1} дн";
+            else if (seconds < 31536000000) return $"{seconds / 31536000:F1} лет";
+            else return "более 1000 лет";
+        }
+
+        private bool IsCommonPassword(string password)
+        {
+            string[] common = {
+                "123456", "password", "123456789", "12345", "12345678",
+                "qwerty", "abc123", "111111", "123123", "admin",
+                "password123", "letmein", "welcome", "monkey", "dragon"
+            };
+            return common.Contains(password.ToLower());
+        }
+
+        private bool IsSequential(string password)
+        {
+            string lower = password.ToLower();
+            string[] seq = {
+                "123", "234", "345", "456", "567", "678", "789",
+                "abc", "bcd", "cde", "def", "efg", "fgh", "ghi",
+                "qwe", "wer", "ert", "rty", "tyu", "yui", "uio",
+                "asd", "sdf", "dfg", "fgh", "ghj", "hjk", "jkl",
+                "zxc", "xcv", "cvb", "vbn", "bnm"
+            };
+            return seq.Any(s => lower.Contains(s));
+        }
+
+        private bool HasRepeatingChars(string password)
+        {
+            for (int i = 0; i < password.Length - 2; i++)
+            {
+                if (password[i] == password[i + 1] && password[i] == password[i + 2])
+                    return true;
+            }
+            return false;
+        }
+
+        private class PasswordEvaluation
+        {
+            public int Score { get; set; }
+            public string Strength { get; set; }
+            public string CrackTime { get; set; }
+        }
+
+        private void lblStrength_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void txtPassword_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
